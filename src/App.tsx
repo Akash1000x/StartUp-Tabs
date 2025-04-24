806bba2712305c01d4405c8dcfc6727b037289e0
Akash kumar kumawat <kumawatap63@gmail.com>
2025-04-24 12:51:20 +0530

    init

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+
+function App() {
+
+  return (
+    <div className="size-[300px] bg-gradient-to-br from-slate-900 to-slate-800 text-white p-6 flex items-center justify-center">
+      <div className="max-w-md mx-auto bg-slate-800/50 rounded-xl shadow-xl backdrop-blur-sm border border-slate-700">
+        <div className="p-5">
+          <h1 className="text-2xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-r from-blue-400 to-violet-500">
+            Browser Extension Template
+          </h1>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ vite.config.ts
@@ -0,0 +1,24 @@
+import path from "path"
+import tailwindcss from "@tailwindcss/vite"
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { crx } from '@crxjs/vite-plugin'
+import manifest from './manifest.json'
+
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    crx({ manifest }),
+    tailwindcss()
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  legacy: {
+    skipWebSocketTokenCheck: true,
+  }
+})
