0f929f0aa32d5428f789cec17633b780592d7203
Akash kumar kumawat <kumawatap63@gmail.com>
2025-08-25 22:27:53 +0530

    Refactor background script to open saved websites on startup

--- src/background.ts
+++ src/background.ts
@@ -1,3 +1,14 @@
-chrome.runtime.onInstalled.addListener(() => {
-  console.log('Background script loaded');
+import { chromeUtils } from './utils/chrome';
+
+chrome.runtime.onStartup.addListener(async () => {
+  try {
+    const { websites } = await chromeUtils.getStorageData();
+
+    for (const website of websites) {
+      await chrome.tabs.create({ url: website.url });
+    }
+
+  } catch (error) {
+    console.error('Error opening websites on startup:', error);
+  }
 });
