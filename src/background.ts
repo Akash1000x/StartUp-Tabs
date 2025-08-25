1441caef61d5e595002646c8720a59f821c6c8a7
Akash kumar kumawat <kumawatap63@gmail.com>
2025-08-25 22:47:45 +0530

    added remove default tab feat.

--- src/background.ts
+++ src/background.ts
@@ -4,8 +4,28 @@ chrome.runtime.onStartup.addListener(async () => {
   try {
     const { websites } = await chromeUtils.getStorageData();
 
-    for (const website of websites) {
-      await chrome.tabs.create({ url: website.url });
+    if (websites.length === 0) return;
+
+    const currentWindow = await chrome.windows.getCurrent();
+    const tabs = await chrome.tabs.query({ windowId: currentWindow.id });
+
+    const defaultTab = tabs.find(tab =>
+      tab.url === 'chrome://newtab/' ||
+      tab.url === 'about:newtab/' ||
+      tab.url === 'chrome://new-tab-page/' ||
+      tab.url === 'edge://newtab/' ||
+      tab.url === 'about:blank'
+    );
+
+    if (defaultTab && websites.length > 0) {
+      for (let i = 0; i < websites.length; i++) {
+        await chrome.tabs.create({ url: websites[i].url });
+      }
+      await chrome.tabs.remove(defaultTab.id!);
+    } else {
+      for (const website of websites) {
+        await chrome.tabs.create({ url: website.url });
+      }
     }
 
   } catch (error) {
